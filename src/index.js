e48a18cb76d1e295ad60a94b59cdce5e4b72c8c4
Jess Pesale <jess_pesale@aol.com>
2021-11-30 14:22:25 -0500

    imported files to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, Provider, Middleware } from 'redux';
+// anything in Provider has access to redux store
+// Middleware incorporate thunk with our store
+import thunk from 'redux-thunk';
 
 import App from './App';
 
+
+// set up store, import provider to use redux
 
 
 ReactDOM.render(
