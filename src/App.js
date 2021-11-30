21e094e30e07c5af3992d554b580e6b5e6c16bb2
Jess Pesale <jess_pesale@aol.com>
2021-11-30 14:17:29 -0500

    played around with fetching the backend to see data in the console

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  
+
   render() {
   return (
     <div className="App">
@@ -13,3 +15,19 @@ class App extends React.Component {
 
 
 export default App;
+
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/trips')
+  //   .then(resp =>resp.json())
+  //   .then(data => console.log(data))
+
+  // }
+
+
+  // fix seeing all packing items or just pakcing items associated wiht a trip at Part 3 32 minutes
+// fetch('http://localhost:3000/api/v1/trips/1/packing_items')
+// accesing all the packing items for a specific trip
+
+// fetch('http://localhost:3000/api/v1/trips/1/itinerary_items')
+// accesing all the itinerary items for a specific trip
