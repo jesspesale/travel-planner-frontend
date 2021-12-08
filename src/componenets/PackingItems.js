eda90d92d235420c604fb896a98cc2d2b2404164
Jess Pesale <jess_pesale@aol.com>
2021-12-08 18:03:53 -0500

    displayed packing items in trip route and made a function in packing items container to check if the trip is loaded or not yet

--- src/componenets/PackingItems.js
+++ src/componenets/PackingItems.js
@@ -2,12 +2,15 @@ import React from "react";
 
 // functional component can receive information from props from PackingContainer
 const PackingItems = (props) => {
-    // console.log(props)
-    return (
-        <div>
-            PackingItems
-        </div>
-    )
+    console.log(props)
+    // if(props.packing_list) {
+            return (
+                <div>
+                   {props.packing_items.map(item => 
+                        <li key={item.id}>{item.item}</li>
+                        )}
+                </div>
+            )
 
 }
 
