a63a8a2d525010cc7642a47c89ae98b521bf05db
Jess Pesale <jess_pesale@aol.com>
2021-12-19 08:50:52 -0500

    added an if statment to only pass trip props if it is not undefined

--- src/componenets/Trip.js
+++ src/componenets/Trip.js
@@ -4,22 +4,36 @@ import TripEdit from "./TripEdit";
 
 const Trip = (props) => {
 
-    console.log(props)
     let trip = props.trips.filter(trip => trip.id == props.match.params.id)[0]
 
-    return (
-        <div>
-            <h3>
-                Destination: {trip ? trip.destination : null}
-                <br/><br/>
-                From {trip ? trip.start_date : null} to {trip ? trip.end_date : null}
-            </h3>
-            <PackingItemContainer trip={trip}/>
-            <br/><br/><br/>
-            <h4>Edit your trip Information:</h4>
-            <TripEdit trip={trip}/>
-        </div>
-    )
+    if (trip) {
+        return (
+            <div>
+                <h3>
+                    Destination: {trip ? trip.destination : null}
+                    <br/><br/>
+                    From {trip ? trip.start_date : null} to {trip ? trip.end_date : null}
+                </h3>
+                <PackingItemContainer trip={trip}/>
+                <br/><br/><br/>
+                <h4>Edit your trip Information:</h4>
+                <TripEdit trip={trip && trip}/>
+            </div>
+        )       
+    }
+    else {
+        return (
+            <div>
+                <h3>
+                    Destination: {trip ? trip.destination : null}
+                    <br/><br/>
+                    From {trip ? trip.start_date : null} to {trip ? trip.end_date : null}
+                </h3>
+                <PackingItemContainer trip={trip}/>
+            </div>    
+        )
+    }
+
 
 }
 
