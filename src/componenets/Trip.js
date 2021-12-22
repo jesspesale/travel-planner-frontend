16dd291d9636de367f5d58c8fdc34698f1af2d55
Jess Pesale <jess_pesale@aol.com>
2021-12-21 21:30:35 -0500

    fixed date format for trip show page

--- src/componenets/Trip.js
+++ src/componenets/Trip.js
@@ -5,24 +5,40 @@ import { deleteTrip } from "../actions/deleteTrip";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
 import {Link} from 'react-router-dom'
-import { Button } from 'react-bootstrap';
+
 
 const Trip = (props) => {
 
     // console.log(props)
     let trip = props.trips.filter(trip => trip.id == props.match.params.id)[0]
-
+    // let startDate = trip.start_date
+    // let endDate = trip.end_date
     const handleDelete = (trip) => {
         props.deleteTrip(trip.id)
     }
 
+    const changeDate = (trip) => {
+        if (trip){
+        let startDate = trip.start_date.split("-")
+        let endDate = trip.end_date.split("-")
+
+        return(
+            `${startDate[1]}/${startDate[2]}/${startDate[0]} -->  ${endDate[1]}/${endDate[2]}/${endDate[0]}`)
+        }
+    }
+
     return (
         <div>
-            <h3>
-                Destination: {trip ? trip.destination : null}
-                <br/><br/>
-                From {trip ? trip.start_date : null} to {trip ? trip.end_date : null}
-            </h3>
+                <h3 className="title">Destination:</h3>
+                 <div className="answer">{trip ? trip.destination : null}</div>
+                <br/>
+                <br/>
+                <h3 className="title">Travel Dates:</h3>
+                <div className="answer">
+                  {trip ? changeDate(trip) : null}
+                  </div>
+                <br/>
+            
             <PackingItemContainer trip={trip}/>
             <br/><br/>
             <Link to={`/trips/`} onClick={trip ? () => handleDelete(trip) : null} >Delete Trip</Link>
