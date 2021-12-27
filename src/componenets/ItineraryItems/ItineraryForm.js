ec03c180339248cc8a0da90ae4dc5f39c9a99d56
Jess Pesale <jess_pesale@aol.com>
2021-12-26 22:34:49 -0500

    started itinerary item form

--- src/componenets/ItineraryItems/ItineraryForm.js
+++ src/componenets/ItineraryItems/ItineraryForm.js
@@ -1,4 +1,7 @@
 import React from "react";
+// import {addItineraryItem} from "../actions/addItineraryItem"
+// import {connect} from "react-redux"
+// import { Button } from 'react-bootstrap';
 
 class ItineraryForm extends React.Component {
 
@@ -13,3 +16,36 @@ class ItineraryForm extends React.Component {
 }
 
 export default ItineraryForm
+
+//     state = {
+//         item: ""
+//     }
+
+//     handleChange = (event) => {
+//         this.setState({
+//             [event.target.name]: event.target.value
+//         })
+//     }
+
+//     handleSubmit = (event) => {
+//         event.preventDefault()
+//         this.props.addPackingItem(this.state, this.props.tripId)
+//         this.setState({
+//             item: ""
+//         })
+//     }
+
+//     render(){
+//         return (
+//             <div>
+//                 Add items to pack:
+//                 <form onSubmit={this.handleSubmit}>
+//                     <label></label>
+//                     <input type="text" name="item" value={this.state.item} placeholder="what to pack" onChange={this.handleChange} ></input>
+//                     <Button className="addButton" type="submit" size="sm" variant="outline-secondary">Add</Button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+// export default connect(null, {addPackingItem})(PackingItemForm)
