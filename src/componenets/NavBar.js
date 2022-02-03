17f776f93c0b93eb8b30d2fc1afb3b538bca5d3d
jesspesale <jess_pesale@aol.com>
2022-02-02 20:44:32 -0500

    added some css for navbar, spaced out All trips and add new trip link

--- src/componenets/NavBar.js
+++ src/componenets/NavBar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div>
+        <div className="navbar">
             <Link to="/trips">All Trips </Link>
-            <br/>
+            
             <Link to="/trips/new">Add a new Trip </Link>
         </div>
     )
